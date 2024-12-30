2a59307008f1538d40424a39f420f07798ca1d25
Mack Richardson <mack@mackrichardson.com>
2024-12-29 21:02:41 -0500

    Added RSS feed and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 import icon from "astro-icon";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon()],
+    site: "https://mackrichardson.com",
+    integrations: [icon(), sitemap()],
     image: {
       service: sharpImageService(),
       domains: ["astro.build"],
